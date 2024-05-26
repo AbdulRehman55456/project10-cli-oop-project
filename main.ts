df47fdce9c548c786a60bfff197b0ee3812aa50d
unknown <you@example.com>
2024-05-26 03:54:04 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+
+class Student {
+  name: string;
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+class Person {
+  students: Student[] = [];
+  addStudent(obj: Student) {
+    this.students.push(obj);
+  }
+}
+
+const persons = new Person();
+const programstart = async (persons: Person) => {
+  do {
+    console.log("Welcome");
+    const ans = await inquirer.prompt({
+      name: "select",
+      type: "list",
+      message: "whom would you like to interact with?",
+      choices: ["staff", "student", "exit"],
+    });
+    if (ans.select === "staff") {
+      console.log(
+        "you approach the staff room. please feel free to ask question"
+      );
+    } else if (ans.select === "student") {
+      const ans = await inquirer.prompt({
+        name: "student",
+        type: "input",
+        message: "Enter the student name you wish to engage with",
+      });
+      const student = persons.students.find((val) => val.name == ans.student);
+      if (!student) {
+        const name = new Student(ans.student);
+        persons.addStudent(name);
+        console.log(`Hello i am ${name.name} nice to meet you!`);
+        console.log("New student added");
+        console.log("Current student list");
+        console.log(persons.students);
+      } else {
+        console.log(`Hello i am ${student.name} nice to see you again!`);
+        console.log("existing students list");
+        console.log(persons.students);
+      }
+    } else if (ans.select === "exit") {
+      console.log("Exiting the program...");
+      process.exit();
+    }
+  } while (true);
+};
+
+programstart(persons);
